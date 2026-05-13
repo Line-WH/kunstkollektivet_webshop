e8d0714e86910bbb123ed3f63bc85abb6be7e954
Line-WH <linerooed.gmail.com>
2026-05-13 08:58:42 +0200

    tilføjet produktbilleder og videoer.

--- js/products.js
+++ js/products.js
@@ -19,6 +19,10 @@ const products = [
                 alt: "Kunstprintet Pige med matcha vist i ramme"
             },
             {
+                src: "images/matcha_3.jpg",
+                alt: "Kunstprintet Pige med matcha vist på væggen"
+            },
+            {
                 src: "images/matcha_4.jpg",
                 alt: "Detaljebillede af kunstprintet Pige med matcha"
             }
@@ -30,6 +34,41 @@ const products = [
             label: "produktvideo"
         }
     },
+
+    {
+        slug: "abstrakte-bjerge-2",
+        title: "Abstrakte bjerge 2",
+        artist: "Nik",
+
+        description: "Bring noget varme og ro ind i hjemmet med det her skønne lille værk af Nana Rooed. Det er perfekt til hvor som helst hvor du mangler farve og roligt spil på din væg.",
+        artistDescription: "Nana Rooed er en kreativ sjæl som holder af at bruge oliekridt og klasiske tucsher. Hendes udtryk bærer derfor også meget præg af teksture og stemninger.",
+        materials: "Printet produceres lokalt i Danmark på kvalitetspapir. Leveringstid og fragt vises ved checkout.",
+
+        images: [
+            {
+                src: "images/abstrakte-bjerge-1.jpg",
+                alt: "digitalt print af bjerge"
+            },
+            {
+                src: "images/abstrakte-bjerge-2.jpg",
+                alt: "digitalt print af bjerge i ramme"
+            },
+            {
+                src: "images/abstrakte-bjerge-3.jpg",
+                alt: "digitalt print af bjerge på væggen"
+            },
+            {
+                src: "images/abstrakte-bjerge-4.jpg",
+                alt: "Digitalt print af bjerge i hånden"
+            }
+        ],
+
+        video: {
+            src: "video/abstrakte-video.mp4",
+            type: "video/mp4",
+            label: "produktvideo"
+        }
+    }
 
 
 ];
